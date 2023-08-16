9c553107fd882e0d738fcc64969a008b6994b975
Dennis Federico <dennisfederico@gmail.com>
2023-08-16 15:52:10 +0200

    add parallel topic creation

---
+++ kubernetes/scripts/create_topics_parallel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Start topic creation job($JOB_COMPLETION_INDEX) of '$TOPIC_COUNT' topics with prefix '$TOPIC_PREFIX-$JOB_COMPLETION_INDEX' ..."
+echo "Using RF='$REPLICATION_FACTOR', partitions='$TOPIC_PARTITIONS', config='$TOPIC_CONFIG'"
+echo "\n"
+
+echo "Start TS... $(date +'%Y-%m-%d %H:%M:%S')"
+start_time=$(date +%s)
+
+for ((i=1; i<=TOPIC_COUNT; i++)); do
+    TOPIC_NAME="${TOPIC_PREFIX}-${JOB_COMPLETION_INDEX}-${i}"
+    kafka-topics.sh \
+      --bootstrap-server $BOOTSTRAP_SERVERS \
+      --command-config /config/properties/connection.properties \
+      --create \
+      --if-not-exists \
+      --topic $TOPIC_NAME \
+      --partitions $TOPIC_PARTITIONS \
+      --replication-factor $REPLICATION_FACTOR
+      #--config $TOPIC_CONFIG
+    echo "Created topic: $TOPIC_NAME"
+    #sleep 0.5
+done
+
+echo "Topic creation completed."
+
+echo "End TS... $(date +'%Y-%m-%d %H:%M:%S')"
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo "Elapsed Time... $elapsed_time seconds"
